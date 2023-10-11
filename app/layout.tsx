fb3466aa9e849b71fbe1d52164ae0a82ad13a09a
Michael Owuor <michael.owuor@gmail.com>
2023-10-11 08:34:54 +0300

    chore: update dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,38 +1,39 @@
-import React, { ReactNode } from 'react';
-import type { Metadata } from 'next';
-import { ClerkProvider } from '@clerk/nextjs';
-import Header from '@/components/header';
-import '@/app/styles/globals.css';
+import React, { ReactNode } from "react";
+import type { Metadata } from "next";
+import { ClerkProvider } from "@clerk/nextjs";
+import Header from "@/components/header";
+import "@/app/styles/globals.css";
 
 type Children = {
-	children: ReactNode;
+  children: ReactNode;
 };
 
 export const metadata: Metadata = {
-	icons: {
-		icon: '/favicon.png',
-		shortcut: '/favicon/png',
-	},
-	title: 'Next.JS + Clerk Auth',
-	description: 'Next.JS application that uses Clerk for authentication and user management.',
+  icons: {
+    icon: "/favicon.png",
+    shortcut: "/favicon/png",
+  },
+  title: "Clerk Auth",
+  description:
+    "Next.JS application that uses Clerk for authentication and user management.",
 };
 
 export default function RootLayout({ children }: Children) {
-	return (
-		<ClerkProvider>
-			<html lang="en">
-				<body className="scroll-smooth antialiased font-opensans">
-					<header>
-						<Header />
-					</header>
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className="scroll-smooth font-opensans antialiased">
+          <header>
+            <Header />
+          </header>
 
-					<main className="container mx-auto">
-						<div className="flex items-start justify-center min-h-screen">
-							<div className="mt-20">{children}</div>
-						</div>
-					</main>
-				</body>
-			</html>
-		</ClerkProvider>
-	);
+          <main className="container mx-auto">
+            <div className="flex min-h-screen items-start justify-center">
+              <div className="mt-20">{children}</div>
+            </div>
+          </main>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
 }
--- components/header.tsx
+++ components/header.tsx
@@ -1,44 +1,52 @@
-import Link from 'next/link';
-import { UserButton, auth } from '@clerk/nextjs';
+import Link from "next/link";
+import { UserButton, auth } from "@clerk/nextjs";
 
 const Header = () => {
-	const { userId } = auth();
+  const { userId } = auth();
 
-	return (
-		<nav className="flex items-center justify-between px-6 py-4 mb-5 bg-sky-800">
-			<div className="flex items-center">
-				<Link href="/">
-					<div className="text-lg font-bold text-white uppercase">Next + Clerk Auth</div>
-				</Link>
-			</div>
+  return (
+    <nav className="mb-5 flex items-center justify-between bg-sky-800 px-6 py-4">
+      <div className="flex items-center">
+        <Link href="/">
+          <div className="text-lg font-bold uppercase text-white">
+            Clerk Auth
+          </div>
+        </Link>
+      </div>
 
-			<div className="flex items-center text-white">
-				{!userId && (
-					<>
-						<Link href="sign-in" className="text-gray-300 hover:text-white mr-4">
-							Sign In
-						</Link>
+      <div className="flex items-center text-white">
+        {!userId && (
+          <>
+            <Link
+              href="sign-in"
+              className="mr-4 text-gray-300 hover:text-white"
+            >
+              Sign In
+            </Link>
 
-						<Link href="sign-up" className="text-gray-300 hover:text-white mr-4">
-							Sign Up
-						</Link>
-					</>
-				)}
+            <Link
+              href="sign-up"
+              className="mr-4 text-gray-300 hover:text-white"
+            >
+              Sign Up
+            </Link>
+          </>
+        )}
 
-				{userId && (
-					<>
-						{/* <Link href="profile" className="text-gray-300 hover:text-white mr-4">
+        {userId && (
+          <>
+            {/* <Link href="profile" className="text-gray-300 hover:text-white mr-4">
 							Profile
 						</Link> */}
 
-						<div className="ml-auto">
-							<UserButton afterSignOutUrl="/" />
-						</div>
-					</>
-				)}
-			</div>
-		</nav>
-	);
+            <div className="ml-auto">
+              <UserButton afterSignOutUrl="/" />
+            </div>
+          </>
+        )}
+      </div>
+    </nav>
+  );
 };
 
 export default Header;
